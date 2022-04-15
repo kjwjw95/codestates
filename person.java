a27b56cc8cd9502fb7aad824316274f55d5f833b
Jungwook Kim <jw0905@naver.com>
2022-04-15 15:43:09 +0900

    Create person.java

---
+++ person.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class Person {
+	public String name;
+	public String mail;
+	public int number;
+	Person(String name,String mail, int number){
+		this.name=name;
+		this.mail=mail;
+		this.number=number;
+	}
+}
